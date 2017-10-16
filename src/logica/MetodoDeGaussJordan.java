8d58996bda2a4eabe694097e5dda67204d54a854
Fernando Celi <fernando_dfcv@hotmail.com>
2017-10-16 01:28:04 -0500

    Se muestra proceso por consola

--- src/logica/MetodoDeGaussJordan.java
+++ src/logica/MetodoDeGaussJordan.java
@@ -17,21 +17,42 @@ public class MetodoDeGaussJordan {
         for (int i = 0; i <= r.length - 1; i++) {
             double d, c = 0;
             d = m[i][i];// seleccionamos el pivote
+            System.out.println(Double.toString(d/2) + "*fila" + (i + 1) + "\n");// muesra en el area de texto el pivote seleccionado
             for (int s = 0; s <= r.length - 1; s++) {// pasamos a convertir en 1 al pivote seleionado
                 m[i][s] = ((m[i][s]) / d);
             }
             r[i] = ((r[i]) / d);
 
+            // paso a mostrar las opraciones realizadas en la matriz aumentada
+            for (int j = 0; j < r.length; j++) {                
+                for (int k = 0; k < r.length; k++) {
+                    System.out.println(Double.toString(m[j][k]) + "\t");
+                }
+                System.out.println("|\t" + Double.toString(r[j]) + "\n");
+            }
+            System.out.println("\n\n");// fin paso a motrar las opraciones realizadas en la matriz aumentada
+
             for (int x = 0; x <= r.length - 1; x++) {
                 if (i != x) {
                     c = m[x][i];
+                    System.out.println("-" + Double.toString(c) + " * fila" + (i + 1) + "+ fila" + (x + 1) + "\n");// mustra en pantalla las opraciones que se realizaran por fila
                     for (int y = 0; y <= r.length - 1; y++) {// se hace cero a todos los elemntos de la colunma que no sean el pivote
                         m[x][y] = m[x][y] - c * m[i][y];
                     }
                     r[x] = r[x] - c * r[i];
+
+                    // paso a mostrar las opraciones realizadas en la matriz aumentada
+                    for (int j = 0; j < r.length; j++) {
+                        for (int k = 0; k < r.length; k++) {
+                            System.out.println(Double.toString(m[j][k]) + "\t");
+                        }
+                        System.out.println("|\t" + Double.toString(r[j]) + "\n");
+                    }
+                    System.out.println("\n\n");// fin paso a motrar las opraciones realizadas en la matriz aumentada
                 }// fin if (i != x)
             }// fin for (int x = 0; x <= r.length - 1; x++)
         }//fin bucle i
         return r;// retorna la solucion l sistema
     }
+
 }
