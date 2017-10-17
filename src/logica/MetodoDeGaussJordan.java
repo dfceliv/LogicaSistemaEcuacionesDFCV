6617f953ff939d649bf932f6d33ba17620270237
Fernando Celi <fernando_dfcv@hotmail.com>
2017-10-16 21:33:24 -0500

    Se agregan puntos de salto en ciclos cuando el valor calculado en alguna iteracion es NaN

--- src/logica/MetodoDeGaussJordan.java
+++ src/logica/MetodoDeGaussJordan.java
@@ -27,7 +27,7 @@ public class MetodoDeGaussJordan {
             if (Double.isNaN(d)) {
                 continue;
             }
-            System.out.println(Double.toString(d/2) + "*fila" + (i + 1) + "\n");// muestra en el area de texto el pivote seleccionado
+            //System.out.println(Double.toString(d/2) + "*fila" + (i + 1) + "\n");// muestra en el area de texto el pivote seleccionado
             for (int s = 0; s <= r.length - 1; s++) {// pasamos a convertir en 1 al pivote seleccionado
                 if (Double.isNaN(m[i][s])) {
                     continue;
@@ -38,20 +38,6 @@ public class MetodoDeGaussJordan {
             if (Double.isNaN(r[i])) {
                 continue;
             }
-            // paso a mostrar las opraciones realizadas en la matriz aumentada
-            for (int j = 0; j < r.length; j++) {                
-                for (int k = 0; k < r.length; k++) {
-                    if (Double.isNaN(m[j][k])) {
-                        continue;
-                    }
-                    System.out.println(Double.toString(m[j][k]) + "\t");
-                }
-                if (Double.isNaN(r[j])) {
-                    continue;
-                }
-                System.out.println("|\t" + Double.toString(r[j]) + "\n");
-            }
-            System.out.println("\n\n");// fin paso a mostrar las opraciones realizadas en la matriz aumentada
 
             for (int x = 0; x <= r.length - 1; x++) {
                 if (i != x) {
@@ -59,29 +45,10 @@ public class MetodoDeGaussJordan {
                     if (Double.isNaN(c)) {
                         continue;
                     }
-                    System.out.println("-" + Double.toString(c) + " * fila" + (i + 1) + "+ fila" + (x + 1) + "\n");// mustra en pantalla las opraciones que se realizaran por fila
                     for (int y = 0; y <= r.length - 1; y++) {// se hace cero a todos los elementos de la colunma que no sean el pivote
                         m[x][y] = m[x][y] - c * m[i][y];
                     }
                     r[x] = r[x] - c * r[i];
-                    
-                    if (Double.isNaN(r[x])) {
-                        continue;
-                    }
-                    // paso a mostrar las opraciones realizadas en la matriz aumentada
-                    for (int j = 0; j < r.length; j++) {
-                        for (int k = 0; k < r.length; k++) {
-                            if (Double.isNaN(m[j][k])) {
-                                continue;
-                            }
-                            System.out.println(Double.toString(m[j][k]) + "\t");
-                        }
-                        if (Double.isNaN(r[j])) {
-                            continue;
-                        }
-                        System.out.println("|\t" + Double.toString(r[j]) + "\n");
-                    }
-                    System.out.println("\n\n");// fin paso a motrar las opraciones realizadas en la matriz aumentada
                 }// fin if (i != x)
             }// fin for (int x = 0; x <= r.length - 1; x++)
         }//fin bucle i
